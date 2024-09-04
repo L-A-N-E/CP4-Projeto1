21e845dadcea873f8afdc9a1360f866bf297144b
Lucas <lucasenzoideyuki@gmail.com>
2024-09-04 16:51:01 -0300

    configurando o style do Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,12 +4,18 @@ import { Link } from 'react-router-dom'
 export const Nav = ({toggleTheme}) => {
     return (
     <>
-    <Link to={'/'}>Home</Link>
-    <Link to={'/produtos'}>Produtos</Link>
-    <Link to={'/sobre'}>Sobre</Link>
-    <Link to={'/contato'}>Contato</Link>
-
-    <button onClick={toggleTheme}>Mudar Tema</button>
+    <header>
+        <div>
+            <Link to={'/'}>Home</Link>
+            <Link to={'/produtos'}>Produtos</Link>
+            <Link to={'/sobre'}>Sobre</Link>
+            <Link to={'/contato'}>Contato</Link>       
+        </div>
+        <div>  
+            <button onClick={toggleTheme}>Mudar Tema</button>
+        </div>
+    </header>
+    
 
     </>
     )
