ea88e46864aee27a77c803ab2076f0f3b72f910e
Alice <alicesb2006@gmail.com>
2024-09-05 17:34:18 -0300

    Criando a estrutura Contato

--- src/routes/Contato.jsx
+++ src/routes/Contato.jsx
@@ -3,7 +3,20 @@ import React from "react"
 const Contato = () =>{
     return(
         <>
-            <h1>Contato</h1>
+            <section>
+                <h1>Contato</h1>
+                <form>
+                    <div className="input">
+                        <label className="label-contato">Nome: </label>
+                        <input type="text" placeholder="Digite o seu nome" className="input-contato" required/>
+                    </div>
+                    <div className="input">
+                        <label className="label-contato">Email: </label>
+                        <input type="email" placeholder="Digite o seu email" className="input-contato" required/>
+                    </div>
+                    <button className="btn-contato">Enviar</button>
+                </form>
+            </section>        
         </>
     )
 }
